5a7c5681a8c1ff7afba23cfd6b0d7342dcd51bf0
pierrelevy <pierrelevy@users.noreply.github.com>
2014-02-03 23:34:33 +0000

    AVATAR-6 : Add a MyDashboardComponent to change avatar into MyDashboard
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/extends/plugin-avatar@57136 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/avatar/web/MyDashboardAvatarComponent.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+
+package fr.paris.lutece.plugins.avatar.web;
+
+import fr.paris.lutece.plugins.avatar.service.AvatarService;
+import fr.paris.lutece.plugins.mydashboard.service.MyDashboardComponent;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.security.LuteceUser;
+import fr.paris.lutece.portal.service.security.SecurityService;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.portal.service.util.AppPropertiesService;
+import fr.paris.lutece.util.html.HtmlTemplate;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * MyDashboard Avatar Component
+ */
+public class MyDashboardAvatarComponent extends MyDashboardComponent
+{
+    private static final String DASHBOARD_COMPONENT_ID = "mydashboard.myDashboardAvatarComponent";
+
+    private static final String MESSAGE_DASHBOARD_COMPONENT_DESCRIPTION = "avatar.mydashboard.component.description";
+
+    private static final String MARK_AVATAR_URL = "avatar_url";
+    private static final String MARK_AVATAR_MODIFY_URL = "avatar_modify_url";
+    private static final String PROPERTY_MODIFY_URL ="avatar.mydashboard.modify.url";
+
+    private static final String TEMPLATE_DASHBOARD_COMPONENT = "skin/plugins/avatar/mydashboard/avatar_component.html";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDashboardData( HttpServletRequest request )
+    {
+        if ( SecurityService.isAuthenticationEnable( ) )
+        {
+            LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+            if ( user != null )
+            {
+                String strAvatarModifyUrl = AppPropertiesService.getProperty(PROPERTY_MODIFY_URL);
+                String strAvatarUrl = AvatarService.getAvatarUrl( user.getUserInfo( LuteceUser.HOME_INFO_ONLINE_EMAIL));
+                
+                Map<String, Object> model = new HashMap<String, Object>( );
+                model.put( MARK_AVATAR_URL, strAvatarUrl );
+                model.put( MARK_AVATAR_MODIFY_URL, strAvatarModifyUrl );
+                HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_DASHBOARD_COMPONENT,
+                        request.getLocale( ), model );
+                return template.getHtml( );
+            }
+        }
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getComponentId( )
+    {
+        return DASHBOARD_COMPONENT_ID;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getComponentDescription( Locale locale )
+    {
+        return I18nService.getLocalizedString( MESSAGE_DASHBOARD_COMPONENT_DESCRIPTION, locale );
+    }
+    
+    
+}
